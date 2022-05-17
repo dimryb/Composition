25a90dac4727710eebe47ac0e17b762c53eaa4d0
dmitry <dimryb@bk.ru>
2022-05-17 23:00:16 +0700

    Создал Domain слой

---
+++ app/src/main/java/space/rybakov/composition/domain/entity/GameSettings.kt
@@ -0,0 +1,9 @@
+package space.rybakov.composition.domain.entity
+
+data class GameSettings (
+    val maxSumValue: Int,
+    val minCountOfRightAnswers: Int,
+    val minPercentOfRightAnswers: Int,
+    val gameTimeInSeconds: Int,
+
+)
--- app/src/main/java/space/rybakov/composition/presentation/MainActivity.kt
+++ app/src/main/java/space/rybakov/composition/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package space.rybakov.composition
+package space.rybakov.composition.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import space.rybakov.composition.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
