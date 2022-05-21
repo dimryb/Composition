310d5efbc0351018e1d3340bef107e05e3676fa2
dmitry <dimryb@bk.ru>
2022-05-21 21:14:01 +0700

    Задействовал binding для доступа к компонентам фрагментов

--- app/src/main/java/space/rybakov/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/space/rybakov/composition/presentation/GameFinishedFragment.kt
@@ -6,14 +6,33 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import space.rybakov.composition.R
+import space.rybakov.composition.databinding.FragmentGameFinishedBinding
+import java.lang.RuntimeException
 
 class GameFinishedFragment : Fragment() {
+
+    private var _binding: FragmentGameFinishedBinding? = null
+    private val binding: FragmentGameFinishedBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game_finished, container, false)
+        _binding = FragmentGameFinishedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonRetry.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/space/rybakov/composition/presentation/GameFragment.kt
+++ app/src/main/java/space/rybakov/composition/presentation/GameFragment.kt
@@ -6,14 +6,32 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import space.rybakov.composition.R
+import space.rybakov.composition.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+
+    private var _binding: FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+    ): View {
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvOption1.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
