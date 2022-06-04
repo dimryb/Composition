642dbb26e1d77c6bdd8905abed1ae754a4926dcd
dmitry <dimryb@bk.ru>
2022-06-04 23:30:17 +0700

    Добавил dataBinding в игровой фрагмент, передавая всю viewModel

--- app/src/main/java/space/rybakov/composition/presentation/BindingAdaptor.kt
+++ app/src/main/java/space/rybakov/composition/presentation/BindingAdaptor.kt
@@ -1,10 +1,19 @@
 package space.rybakov.composition.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import space.rybakov.composition.R
 import space.rybakov.composition.domain.entity.GameResult
+
+interface OnOptionClickListener {
+
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -38,16 +47,48 @@ fun bindScorePercentage(textView: TextView, gameResult: GameResult) {
     )
 }
 
-private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult){
+private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) {
     if (countOfQuestions == 0) {
         0
     } else {
-        ((countOfRightAnswers / countOfQuestions.toDouble())* 100).toInt()
+        ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
     }
 }
 
 @BindingAdapter("smile")
-fun bindSmile(imageView: ImageView, winner: Boolean){
+fun bindSmile(imageView: ImageView, winner: Boolean) {
     val resId = if (winner) R.drawable.ic_smile else R.drawable.ic_sad
     imageView.setImageResource(resId)
+}
+
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enough))
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorRedId = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorRedId)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener){
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
+    }
 }
